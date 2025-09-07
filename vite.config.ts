2145345459c2f5c5fd33481daf1667b2a544f1c4
HarryJamesGreenblatt <HarryJamesGreenblatt@ServiceCentricSolutions.com>
2025-09-06 22:20:04 -0700

    fix: Update build output location and environment variable names

--- vite.config.ts
+++ vite.config.ts
@@ -8,8 +8,8 @@ export default defineConfig({
     outDir: 'build'
   },
   define: {
-    'process.env.REACT_APP_API_URL': JSON.stringify(process.env.REACT_APP_API_URL),
-    'process.env.REACT_APP_CLIENT_ID': JSON.stringify(process.env.REACT_APP_CLIENT_ID),
-    'process.env.REACT_APP_TENANT_ID': JSON.stringify(process.env.REACT_APP_TENANT_ID)
+    'process.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL),
+    'process.env.VITE_EXTERNAL_CLIENT_ID': JSON.stringify(process.env.VITE_EXTERNAL_CLIENT_ID),
+    'process.env.VITE_EXTERNAL_TENANT_ID': JSON.stringify(process.env.VITE_EXTERNAL_TENANT_ID)
   }
 })
